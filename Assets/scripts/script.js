e5a4c218e2d5df22a77cc5dd3eb2e739e7f62798
DillanThomas <mansor.dillan@gmail.com>
2021-09-29 14:42:42 -0700

    updated application

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,3 +1,4 @@
+
 // -------------THOUGHTS------------------
 // listen for generate button
 // check to see if check mark boxes are checked
@@ -7,24 +8,29 @@
 // add password length to for loop
 // in for loop, generate random numbers within array.length
 // print password into text box.
-const btnGenerate = document.getElementById("generate");
-const cmkUppercase = document.getElementById("uppercase");
-const cmkLowercase = document.getElementById("lowercase");
-const cmknumbers = document.getElementById("numbers");
-const cmksymbols = document.getElementById("symbols");
-const txtLength = document.getElementById("total-characters");
+
+const btnGenerate = document.querySelector("#generate");
+const cmkUppercase = document.querySelector("#uppercase");
+const cmkLowercase = document.querySelector("#lowercase");
+const cmknumbers = document.querySelector("#numbers");
+const cmksymbols = document.querySelector("#symbols");
+const txtLength = document.querySelector("#total-characters");
+const includedword = document.querySelector("#additional");
+const inputfield = document.querySelector("#password");
+inputfield.style.color = "rgb(159, 255, 159)";
 // -----------GLOBALS---------------------
 var group;
 var lowerCase;
 var upperCase;
 var numbers;
 var symbols;
-var boolUppercase = false;
-var boolLowercase = false;
-var boolNumbers = false;
-var boolSymbols = false;
+var boolUppercase;
+var boolLowercase;
+var boolNumbers;
+var boolSymbols;
+
 // ---------------LOAD---------------------------
-let startUp = new function(){
+let getCharacters = new function(){
   var x = 'abcdefghijklmnopqrstuvwxyz';
   var y = '12345678901234567890';
   var z = '`~!@#$%^&*()_+=|][:?><,./';
@@ -33,36 +39,49 @@ let startUp = new function(){
   upperCase = a.split('');
   numbers = y.split('');
   symbols = z.split('');
-  group = [upperCase,lowerCase,numbers,symbols];  
-}
+  group = [upperCase,lowerCase,numbers,symbols];
+  }
+
 // --------------LISTENS------------------------------------------------------------
 cmkUppercase.addEventListener('click',function(){boolUppercase = !boolUppercase });
 cmkLowercase.addEventListener('click',function(){ boolLowercase = !boolLowercase });
 cmknumbers.addEventListener('click',function(){ boolNumbers = !boolNumbers });
 cmksymbols.addEventListener('click',function(){ boolSymbols = !boolSymbols });
+
 btnGenerate.addEventListener("click", function() {
-  // local-vars
-  var bools = [boolUppercase,boolLowercase,boolNumbers,boolSymbols];
-  var pool = []; 
+  var pool = [];
   var result = [];
-  var characterTotal;
-  var inputfield = document.getElementById("password");
+  var bools = [boolUppercase, boolLowercase, boolNumbers, boolSymbols];  
+  var characterTotal = txtLength.value;
+  var additionaltext = includedword.value;
+
   // activity
-  if(txtLength.value != isNaN(txtLength.value)){
-    characterTotal = txtLength.value;
-  } else { alert("Enter a correct password length please.")}
-  for (i = 0; i < bools.length; i++) {
-    if(bools[i] === true) {pool = pool.concat(group[i])};
-  }
-  if(pool.length === 0) {pool = pool.concat(group[1])};  
+
+  addCharacterGroupsToThePool();
+
   if(characterTotal > 8 && characterTotal < 75) {
-    for ( var i = 0; i < characterTotal; i++ ) {
-      var randomnumber = Math.floor(Math.random() * pool.length)
-      result = result.concat(pool[randomnumber])
-      inputfield.textContent = "" + "\n" + result.join("");
-      inputfield.style.color = "rgb(159, 255, 159)";
+    var i = generatePassword();
+  } else { alert("Enter a correct password length please.")}  
+
+  function addCharacterGroupsToThePool() {
+    for (i = 0; i < bools.length; i++) {
+      if (bools[i] === true) {
+        pool = pool.concat(group[i]);
+      };
     }
-  } else { alert("Enter a correct password length please.")}
+    if (pool.length === 0) {
+      pool = lowerCase;
+    }
+  }
+  function generatePassword() {
+    for (var i = 0; i < characterTotal; i++) {
+      var randomnumber = Math.floor(Math.random() * pool.length);
+      result = result.concat(pool[randomnumber]);
+    }
+    inputfield.textContent = "\n" + additionaltext + result.join("");
+    return i;
+  }
 });
+
 
 
